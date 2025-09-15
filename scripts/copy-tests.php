6728db450f1a90c93ad0df20e89f9deabf52bd80
D-Bobrikov <dmitrii.bobrikov1988@gmail.com>
2025-09-15 22:30:01 +0700

    add tests

---
+++ scripts/copy-tests.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Рекурсивно копирует содержимое каталога $src в $dst
+ */
+function copy_recursive($src, $dst)
+{
+    $dir = opendir($src);
+    @mkdir($dst, 0777, true);
+    while (false !== ($file = readdir($dir))) {
+        if (($file != '.') && ($file != '..')) {
+            if (is_dir($src.'/'.$file)) {
+                copy_recursive($src.'/'.$file, $dst.'/'.$file);
+            } else {
+                copy($src.'/'.$file, $dst.'/'.$file);
+            }
+        }
+    }
+    closedir($dir);
+}
+
+$src = __DIR__.'/../publishable/tests';
+$dst = __DIR__.'/../tests/vendor/command-processor';
+if (is_dir($src)) {
+    copy_recursive($src, $dst);
+    echo "Tests copied to $dst\n";
+} else {
+    echo "Source tests directory not found: $src\n";
+}
